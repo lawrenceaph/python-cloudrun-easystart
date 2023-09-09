6c45f63f5125c0181a71a8d0ebf31dc3535cf89a
Lawrence Aritao <lawrence.aritao@gmail.com>
2023-09-09 03:21:36 +0000

    stylized

--- app.py
+++ app.py
@@ -4,7 +4,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Dockerized Flask!"
+    return HTML_TEMPLATE
 
-    if __name__ == '__main__':
-            app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+
+
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flask on Google Cloud Run</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: 'sans-serif';
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f4f4f4;
+        }
+        h1 {
+            font-size: 44px;
+            font-family:'arial';
+            color: #0079c1;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+<body>
+    <h1>Flask on Google Cloud Run</h1>
+</body>
+</html>
+"""
