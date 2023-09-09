bec3d33b0fdb96d4fd5cbcd4ad1b4cb2b8e3b18c
Lawrence Aritao <lawrence.aritao@gmail.com>
2023-09-09 05:33:21 +0000

    gist controlled

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -6,8 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    url = "https://gist.githubusercontent.com/lawrenceaph/532ae1423a43d1b17ee41780f9a93d18/raw/55dc8fac2044a5c9d7954b7287bb627b5ec73ead/message.txt"
+    response = requests.get(url)
     title = "Flask on Google Cloud Run 🎉"
-    return render_template('index.html', page_title=title, content=title) 
+    message = response.text or "hi"
+    return render_template('index.html', page_title=title, content=title, message = message) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
